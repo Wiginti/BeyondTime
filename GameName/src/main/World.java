e3ae9e95a025debd789b732de943e86cb7688c8f
Wiginti <marcodupre60@gmail.com>
2024-11-12 09:36:43 +0100

    Main class added, location names supported in World

--- GameName/src/main/World.java
+++ GameName/src/main/World.java
@@ -1,7 +1,11 @@
 package main;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class World {
 	
+	private static final List<String> locationNames = Arrays.asList("name1", "name2", "name3", "name4", "name5");
 	public static World myWorld = new World();
 	private Location[] locations;
 	
@@ -10,12 +14,11 @@ public class World {
 	private World() {
 		this.locations = new Location[NUMBER_LOCATIONS];
 		for(int i = 0; i < World.NUMBER_LOCATIONS; i++) {
-			locations[i] = new Location(i);
+			locations[i] = new Location(i, World.locationNames.get(i));
 		}
-		
 	}
 	
-	public World getWorld() {
+	public static World getWorld() {
 		return World.myWorld;
 	}
 
