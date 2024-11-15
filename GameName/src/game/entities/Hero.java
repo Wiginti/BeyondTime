aebfbdf31c888342ef58d4ecb4fd4a85aed731da
Wiginti <marcodupre60@gmail.com>
2024-11-15 15:34:27 +0100

    Add attribute heroLocation with his getter and setter

--- GameName/src/game/entities/Hero.java
+++ GameName/src/game/entities/Hero.java
@@ -1,18 +1,23 @@
 package game.entities;
 
+import game.map.Location;
+import game.map.World;
+
 public class Hero {
 	
 	private int healthPoint;
 	private String name;
 	private Bag bag;
+	private Location heroLocation;
 	
 	public static final int DEFAULT_HEATLH = 100;
+	public static final Location DEFAULT_LOCATION = World.getWorld().getLocation(0);
 	
 	public Hero(String myName) {
 		this.healthPoint = Hero.DEFAULT_HEATLH;
 		this.name = myName;
 		this.bag = new Bag();
-		
+		this.heroLocation = DEFAULT_LOCATION;
 	}
 	
 	public String getName() {
@@ -41,6 +46,16 @@ public class Hero {
 	
 	public Bag getBag() {
 		return this.bag;
+	}
+	
+	//Getter and and Setter for heroLocation
+	
+	public void setHeroLocation(Location location) {
+		this.heroLocation = location;
+	}
+	
+	public Location getHeroLocation() {
+		return this.heroLocation;
 	}
 	
 	
