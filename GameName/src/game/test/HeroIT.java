a795604811bd4bd61840c8b0f03bd26e9e819108
Ruubinho <166310534+Ruubinho@users.noreply.github.com>
2024-12-03 11:02:46 +0100

    Create HeroIt.java

---
+++ GameName/src/game/test/HeroIT.java
@@ -0,0 +1,41 @@
+package game.test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.Test;
+
+import game.entities.Bag;
+import game.entities.Hero;
+
+public class HeroIT {
+    
+    @Test
+    void testHeroConstructor() {
+        // Arrange
+        String expectedName = "TestHero";
+        int expectedHealthPoint = Hero.DEFAULT_HEATLH;
+
+        // Act
+        Hero hero = new Hero();
+
+        // Assert
+        assertEquals(expectedName, hero.getName(), "Hero name should match the expected name.");
+        assertEquals(expectedHealthPoint, hero.getHealth(), "Hero health points should match the default value.");
+        assertNotNull(hero.getBag(), "Hero bag should not be null.");
+        assertNotNull(hero.getHeroLocation(), "Hero location should not initially be null.");
+    }
+
+    @Test
+    void testGetBag() {
+        
+        Hero hero = new Hero();
+
+        
+        Bag bag = hero.getBag();
+
+        // Assert
+        assertNotNull(bag, "The bag should not be null.");
+    }
+    
+}
